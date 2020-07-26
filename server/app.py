030719f6d1820b5dbb5d540bca28c294704b7a91
Maria Mihu <mmihu@uwaterloo.ca>
2020-07-25 20:32:02 -0400

    takes URL & passes it through the API

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from mechanize import Browser
 import sys
@@ -14,29 +14,35 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-#WORKING WITH THE CONTENT ON THE WEBSITE
-link = 'https://en.wikipedia.org/wiki/Bee_Movie' #yes i am using the plot of the bee movie for the sample text lmao
+def summarizeArticle(articleURL):
+    br = Browser()
+    br.open(articleURL)
+    article_title = br.title()
 
-br = Browser()
-br.open(link)
-article_title = br.title()
+    model = 'IAB_en' #basically specifies that the language is english
+    key = '9cb4dbf4b67056958232d52630d21988' #to access any API you need to pass your personal key into the request. This is our key
+    summary = ''
 
-model = 'IAB_en' #basically specifies that the language is english
-key = '9cb4dbf4b67056958232d52630d21988' #to access any API you need to pass your personal key into the request. This is our key
-summary = ''
+    summarization_response = meaningcloud.SummarizationResponse(meaningcloud.SummarizationRequest(key, sentences=3, url=articleURL).sendReq())
+    if summarization_response.isSuccessful():
+        summary = summarization_response.getSummary()
+        print(summary)
+    else:
+        print("\tOops! Request to Summarization was not succesful: (" + summarization_response.getStatusCode() + ') ' + summarization_response.getStatusMsg())
 
-summarization_response = meaningcloud.SummarizationResponse(meaningcloud.SummarizationRequest(key, sentences=3, url=link).sendReq())
-if summarization_response.isSuccessful():
-    summary = summarization_response.getSummary()
-    print(summary)
-else:
-    print("\tOops! Request to Summarization was not succesful: (" + summarization_response.getStatusCode() + ') ' + summarization_response.getStatusMsg())
 
-# OPEN http://localhost:5000/ ON YOUR DEVICE AND IT SHOULD DISPLAY THE STRING
-@app.route('/', methods=['GET'])
-def ping_pong():
-    return jsonify(summary)
+#READING IN THE URL PASSED IN FROM THE FRONT
+link = '' #using this page as a placeholder for now
 
+@app.route('/', methods=['GET','POST'])
+def test():
+    if request.method == 'POST':
+        requestData = request.get_json()
+        link = requestData['URL']
+        summarizeArticle(link)
+        return jsonify('IT WORKS')
+    else:
+        return jsonify('not yet')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="localhost", port=8081, debug=True)
